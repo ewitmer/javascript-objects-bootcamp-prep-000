e9b2edaa09ba9701488bd9759338c22f17a7bcd9
Erin Witmer <ecwitmer@gmail.com>
2017-12-07 03:14:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {U2: "The Blackout"}
 
 function updatePlaylist(playlist, name, title) {
-  return Object.assign(playlist, {name: title})
+  return Object.assign(playlist, {[name]: title})
 } 
