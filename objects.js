d4c05adb76a9a5fb258ef655f6c3d899cf4173a7
Erin Witmer <ecwitmer@gmail.com>
2017-12-07 03:16:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,4 +4,7 @@ function updatePlaylist(playlist, name, title) {
   return Object.assign(playlist, {[name]: title})
 } 
 
-
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
